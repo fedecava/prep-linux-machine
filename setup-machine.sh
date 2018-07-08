486ab58eeeae0a9942c2d79839ab3abb12a4bf40
fedecava <federico.cavaletto@gmail.com>
2018-07-08 21:40:09 +0200

    Update setup-machine.sh

--- setup-machine.sh
+++ setup-machine.sh
@@ -3,18 +3,18 @@
 echo "Prepping this machine"
 
 echo "moving config files to home folder"
-cd ../
-mv /prep-linux-machine/.bash_profile ~
-mv /prep-linux-machine/.tmux.conf ~
+mv ~/prep-linux-machine/.bash_profile ~
+mv ~/prep-linux-machine/.tmux.conf ~
 
 echo "Creating scratch-area in home folder"
 mkdir ~/scratch-area
 
-echo "removing this directory and its content"
-rm -rf /prep-linux-machine
+echo "removing directory and its content"
+rm -rf ~/prep-linux-machine
 
 echo "Downloading tmux plugin manager"
-cd ~
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-echo "All done."
+cd ~
+
+echo "All done!"
