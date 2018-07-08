7068fa06026ec6e80efea22583331ce20a078f51
fedecava <federico.cavaletto@gmail.com>
2018-07-08 20:57:12 +0200

    Update setup-machine.sh

--- setup-machine.sh
+++ setup-machine.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 echo "Prepping this machine"
-cd ~
+cd ../
+mv /prep-linux-machine/.bash_profile ~
+mv /prep-linux-machine/.tmux.conf ~
+rm -r /prep-linux-machine
+
 
 echo "Downloading tmux plugin manager"
+cd ~
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo "All done!"
